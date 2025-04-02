b456e2bdf8051a57b551d01cd4dfa62eb6a705eb
AKANKSHA3110 <akankshasrivastava10312005@gmail.com>
2025-04-03 00:58:40 +0530

    Initial commit

---
+++ scope.js
@@ -0,0 +1,22 @@
+function one(){
+    const username ="Gogo"
+
+    function two(){
+        const website ="Youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username = "Dodo"
+    if(username==="Dodo"){
+      const website ="Youtube"
+    //   console.log(username+website)
+    //   console.log(website)
+    }
+    // console.log(username)
+}
